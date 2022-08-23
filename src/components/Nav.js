38e6a4018e7cfb40c5bba78fac108ecd51ce8627
ibizugbe <abrahamibizugbe@gmail.com>
2022-08-23 10:09:53 +0100

    fix linter errors

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./nav.css";
-import planet from "./assets/planet.png";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './nav.css';
+import planet from './assets/planet.png';
 
 const Nav = () => (
   <header>
