5530b020c9818f3df6121720341009d8840ba3f9
ibizugbe <abrahamibizugbe@gmail.com>
2022-08-23 14:50:37 +0100

    add store

---
+++ src/Redux/rockets/Rocket.js
@@ -0,0 +1,7 @@
+const ADD_RESERVATION = "space-travelers/rockets/ADD_ROCKET_RESERVATION";
+const REMOVE_RESERVATION = "space-travelers/rockets/REMOVE__ROCKET_RESERVATION";
+const GET_ROCKETS = "space-travelers/rockets/GET_ROCKETS";
+const apiURL = "https://api.spacexdata.com/v3/rockets";
+
+// store
+const initialState = [];
