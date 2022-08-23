fd0ddc01b6059c90626149a8588f9a24b47fa540
ibizugbe <abrahamibizugbe@gmail.com>
2022-08-23 15:13:36 +0100

    add redux thunk to project dependency

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,7 +1,7 @@
-import { configureStore, combineReducers, applyMiddleware } from "redux";
-import logger from "redux-logger";
-import thunk from "redux-thunk";
-import rocketReducer, { fetchRocketsAPI } from "./rockets/rockets";
+import { configureStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import rocketReducer, { fetchRocketsAPI } from './rockets/rockets';
 
 const reducer = combineReducers({
   rocketReducer,
