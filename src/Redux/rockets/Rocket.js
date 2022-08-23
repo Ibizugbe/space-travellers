bf3f22976261c11687c1e3f7ba5dc3dd3dc3cac5
ibizugbe <abrahamibizugbe@gmail.com>
2022-08-23 12:30:07 +0100

    add function to fetch API

--- src/Redux/rockets/Rocket.js
+++ src/Redux/rockets/Rocket.js
@@ -21,3 +21,24 @@ export const getRockets = (payload) => ({
   type: GET_ROCKETS,
   payload,
 });
+
+// fetch api
+
+export const fetchRocketsAPI = () => async (dispatch) => {
+  await fetch(`${apiURL}`)
+    .then((response) => response.json())
+    .then((rocketList) => {
+      console.log("rocketList: ", rocketList);
+
+      const arrangedList = rocketList.map((rocket) => ({
+        id: rocket.rocket_id,
+        name: rocket.rocket_name,
+        type: rocket.rocket_type,
+        flickr_images: rocket.flickr_images,
+        reservation: false,
+      }));
+      if (arrangedList) {
+        dispatch(getRockets(arrangedList));
+      }
+    });
+};
