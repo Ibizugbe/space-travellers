2e6c2e73454707b0b262014cee6a8b525d6f8744
ibizugbe <abrahamibizugbe@gmail.com>
2022-08-23 15:18:14 +0100

    fix linter errors, rename rocket import extension

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,7 +1,7 @@
 import { configureStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import rocketReducer, { fetchRocketsAPI } from './rockets/rockets';
+import rocketReducer, { fetchRocketsAPI } from './rockets/rocket';
 
 const reducer = combineReducers({
   rocketReducer,
