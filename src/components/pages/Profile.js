9978d222f2402a193933068806891c4d88b7f1e9
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-08-24 21:27:02 +0100

    render rocket reservation in profile

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
+import RocketProfile from "../rockets/RocketProfile";
 
-const Profile = () => (
-  <>
-    <main>
-      <h1>Profile</h1>
-    </main>
-  </>
-);
+const Profile = () => {
+  return (
+    <>
+      <main>
+        <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th>My Rocket Reservations</th>
+              </tr>
+            </thead>
+            <tbody>
+              {profileRocket.length ? (
+                profileRocket.map((item) => (
+                  <RocketProfile key={item.id} id={item.id} name={item.name} />
+                ))
+              ) : (
+                <tr className="empty">
+                  <td>No Rockets have been reserved</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      </main>
+    </>
+  );
+};
 
 export default Profile;
--- src/components/rockets/RocketProfile.js
+++ src/components/rockets/RocketProfile.js
@@ -5,12 +5,21 @@ import { removeRocketReservation } from "../../redux/rockets/rockets";
 
 function RocketProfile() {
   const dispatch = useDispatch();
-  const { id, name, wikipedia } = props;
+  const { id, name } = props;
 
-    const removeRocketReservation = () => {
-        dispatch(removeRocketReservation(id));
-    };
-  return (  );
+  const removeRocketReservation = () => {
+    dispatch(removeRocketReservation(id));
+  };
+  return (
+    <li className="reserved-rocket" id={id}>
+      <h3>{name}</h3>
+    </li>
+  );
 }
+
+RocketProfile.propTypes = {
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+};
 
 export default RocketProfile;
