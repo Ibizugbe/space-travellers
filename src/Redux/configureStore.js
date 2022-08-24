ef7085d9e6788256827b8c25da6bd02185444662
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-08-24 16:34:21 +0100

    fix linter errors

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -5,7 +5,6 @@ import {
 } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import rocketReducer, { fetchRocketsAPI } from './rockets/rocket';
 import missionsReducer from './missions/Mission';
 
