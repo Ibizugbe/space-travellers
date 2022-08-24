d242687b6152547fe8fde33374f62db8a7d33e12
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-08-24 16:02:46 +0100

    fix linter errors

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,13 +1,8 @@
+import {
+  applyMiddleware,
+  combineReducers,
+  configureStore,
 } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
-import rocketReducer, { fetchRocketsAPI } from './rockets/rocket';
-import missionsReducer from './missions/Mission';
-
-const rootReducer = combineReducers({
-  mission: missionsReducer,
-  rock  { reducer: royMiddleware(thunk, logger),
-
-import thunk from 'redux-thunk';""""""""""} from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rocketReducer, { fetchRocketsAPI } from './rockets/rocket';
@@ -21,9 +16,7 @@ const rootReducer = combineReducers({
 const myStore = configureStore(
   { reducer: rootReducer },
   applyMiddleware(thunk, logger),
-import {
-  applyMiddleware,
-  combineReducers,
 );
 myStore.dispatch(fetchRocketsAPI());
 
+export default myStore;
