8008de2ff1a369652ed7d4bfa8f12c871627451c
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-08-26 19:05:42 +0100

    fix linter errors

--- src/__tests__/mission.test.js
+++ src/__tests__/mission.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from '../redux/configureStore';
+import store from '../Redux/configureStore';
 import Mission from '../components/pages/Mission';
 
 it('renders correctly', () => {
