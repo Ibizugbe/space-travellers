0cc3c7aad991b3e8b97e7f99b7beb74300150745
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-08-26 21:06:45 +0100

    fix linter errors

--- src/__tests__/mission.test.js
+++ src/__tests__/mission.test.js
@@ -1,11 +1,11 @@
-import React from "react";
-import Renderer from "react-test-renderer";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import myStore from "../Redux/configureStore";
-import Mission from "../components/pages/Mission";
+import React from 'react';
+import Renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import myStore from '../Redux/configureStore';
+import Mission from '../components/pages/Mission';
 
-it("renders correctly", () => {
+it('renders correctly', () => {
   const tree = Renderer.create(
     <React.StrictMode>
       <Provider store={myStore}>
@@ -13,7 +13,7 @@ it("renders correctly", () => {
           <Mission />
         </BrowserRouter>
       </Provider>
-    </React.StrictMode>
+    </React.StrictMode>,
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
--- src/__tests__/rocket.test.js
+++ src/__tests__/rocket.test.js
@@ -1,11 +1,11 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import myStore from "../Redux/configureStore";
-import Rockets from "../components/pages/Rockets";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import myStore from '../Redux/configureStore';
+import Rockets from '../components/pages/Rockets';
 
-it("renders correctly", () => {
+it('renders correctly', () => {
   const tree = renderer
     .create(
       <React.StrictMode>
@@ -14,7 +14,7 @@ it("renders correctly", () => {
             <Rockets />
           </BrowserRouter>
         </Provider>
-      </React.StrictMode>
+      </React.StrictMode>,
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
