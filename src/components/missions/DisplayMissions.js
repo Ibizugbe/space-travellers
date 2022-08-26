f7c14116abc67f3420a52d1456dad2c1a2645813
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-08-26 12:28:47 +0100

    add style to mission page

--- src/components/missions/DisplayMissions.js
+++ src/components/missions/DisplayMissions.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { PropTypes } from 'prop-types';
-import { joinMission, leaveMission } from '../../Redux/missions/Mission';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { PropTypes } from "prop-types";
+import { joinMission, leaveMission } from "../../Redux/missions/Mission";
 
 const DisplayMission = (props) => {
-  const {
-    id, name, description, reserved,
-  } = props;
+  const { id, name, description, reserved } = props;
   const dispatch = useDispatch();
 
   const handleJoinMission = () => {
@@ -19,18 +17,30 @@ const DisplayMission = (props) => {
 
   return (
     <tr>
-      <th>{name}</th>
-      <td>{description}</td>
+      <th scope="row">{name}</th>
+      <td className="col-lg-7">{description}</td>
       <td>
-        {reserved ? <span>Active Member</span> : <span>Not A Member</span>}
+        {reserved ? (
+          <span className="badge text-bg-primary">Active Member</span>
+        ) : (
+          <span className="badge text-bg-secondary">Not A Member</span>
+        )}
       </td>
       <td>
         {reserved ? (
-          <button type="button" onClick={handleLeaveMission}>
+          <button
+            type="button"
+            className="btn btn-outline-danger btn-sm"
+            onClick={handleLeaveMission}
+          >
             Leave Mission
           </button>
         ) : (
-          <button type="button" onClick={handleJoinMission}>
+          <button
+            type="button"
+            className="btn btn-outline-secondary test-small btn-sm"
+            onClick={handleJoinMission}
+          >
             Join Mission
           </button>
         )}
--- src/components/pages/Mission.js
+++ src/components/pages/Mission.js
@@ -6,26 +6,28 @@ import "bootstrap/dist/css/bootstrap.css";
 const Mission = () => {
   const missions = useSelector((state) => state.mission);
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Missions</th>
-          <th>Description</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {missions.map((mission) => (
-          <DisplayMission
-            name={mission.name}
-            key={mission.id}
-            id={mission.id}
-            description={mission.description}
-            reserved={mission.reserved}
-          />
-        ))}
-      </tbody>
-    </table>
+    <div className="container-fluid px-5">
+      <table className="table table-hover table-striped table-responsive">
+        <thead>
+          <tr>
+            <th scope="col">Missions</th>
+            <th scope="col">Description</th>
+            <th scope="col">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map((mission) => (
+            <DisplayMission
+              name={mission.name}
+              key={mission.id}
+              id={mission.id}
+              description={mission.description}
+              reserved={mission.reserved}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
