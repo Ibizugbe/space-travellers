6f024f3349b6b2ef998b6668bdda08dffde5eb4e
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-08-26 18:58:54 +0100

    fix linter errors

--- src/__tests__/mission.test.js
+++ src/__tests__/mission.test.js
@@ -1,11 +1,11 @@
-import React from "react";
-import Renderer from "react-test-renderer";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "../redux/configureStore";
-import Mission from "../components/pages/Mission";
+import React from 'react';
+import Renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
+import Mission from '../components/pages/Mission';
 
-it("renders correctly", () => {
+it('renders correctly', () => {
   const tree = Renderer.create(
     <React.StrictMode>
       <Provider store={store}>
@@ -13,7 +13,7 @@ it("renders correctly", () => {
           <Mission />
         </BrowserRouter>
       </Provider>
-    </React.StrictMode>
+    </React.StrictMode>,
   ).toJSON();
   expect(tree).toMatchSnapshot(`
 <div
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import "./nav.css";
-import planet from "./assets/planet.png";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './nav.css';
+import planet from './assets/planet.png';
 
 const Nav = () => (
   <header>
