c0e7d8a58d6600be2bf5b3f461acf9453c470406
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-08-26 21:06:13 +0100

    run test

--- src/__tests__/mission.test.js
+++ src/__tests__/mission.test.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Renderer from 'react-test-renderer';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import myStore from '../Redux/configureStore';
-import Mission from '../components/pages/Mission';
+import React from "react";
+import Renderer from "react-test-renderer";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import myStore from "../Redux/configureStore";
+import Mission from "../components/pages/Mission";
 
-it('renders correctly', () => {
+it("renders correctly", () => {
   const tree = Renderer.create(
     <React.StrictMode>
       <Provider store={myStore}>
@@ -13,36 +13,7 @@ it('renders correctly', () => {
           <Mission />
         </BrowserRouter>
       </Provider>
-    </React.StrictMode>,
+    </React.StrictMode>
   ).toJSON();
-  expect(tree).toMatchInlineSnapshot(`
-<div
-  className="container-fluid px-5"
->
-  <table
-    className="table table-hover table-striped table-responsive"
-  >
-    <thead>
-      <tr>
-        <th
-          scope="col"
-        >
-          Missions
-        </th>
-        <th
-          scope="col"
-        >
-          Description
-        </th>
-        <th
-          scope="col"
-        >
-          Status
-        </th>
-      </tr>
-    </thead>
-    <tbody />
-  </table>
-</div>
-`);
+  expect(tree).toMatchSnapshot();
 });
--- src/__tests__/rocket.test.js
+++ src/__tests__/rocket.test.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import Renderer from 'react-test-renderer';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import myStore from '../Redux/configureStore';
-import Rockets from '../components/pages/Rockets';
+import React from "react";
+import renderer from "react-test-renderer";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import myStore from "../Redux/configureStore";
+import Rockets from "../components/pages/Rockets";
 
-it('renders correctly', () => {
-  const tree = Renderer.create(
-    <React.StrictMode>
-      <Provider store={myStore}>
-        <BrowserRouter>
-          <Rockets />
-        </BrowserRouter>
-      </Provider>
-    </React.StrictMode>,
-  ).toJSON();
-  expect(tree).toMatchInlineSnapshot(`
-<ul
-  className="my-rocket-list"
-/>
-`);
+it("renders correctly", () => {
+  const tree = renderer
+    .create(
+      <React.StrictMode>
+        <Provider store={myStore}>
+          <BrowserRouter>
+            <Rockets />
+          </BrowserRouter>
+        </Provider>
+      </React.StrictMode>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
